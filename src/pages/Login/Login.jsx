5912ecd3e9d796739f94c00687b57ac3ade398a9
faryadicka <faryadicka98@gmail.com>
2022-08-22 11:22:12 +0700

    fix modal centered login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -201,7 +201,7 @@ function SignIn() {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title text-center" id="exampleModalLabel">
